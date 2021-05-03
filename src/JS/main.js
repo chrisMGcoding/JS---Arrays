c7421128aaa9b6d2b404f4060dd9c632a3367a79
ChrisMGcoding <chris.degroef@gmail.com>
2021-05-03 10:34:16 +0200

    JS - Array (start)

---
+++ src/JS/main.js
@@ -0,0 +1,7 @@
+let maVar = "ceci est une variable";
+
+let semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+console.log(semaine);
+console.log(semaine[2]);
+console.log(semaine[1] + " " + semaine[5]);
+console.log(semaine[7]);
